cda6ed740e314c919b8a88518cfd61a2dc712636
repelliuss <repelliuss@gmail.com>
2026-09-14 12:46:24 +0300

    disable ssh-agency on android

--- local/enable/tools/ssh-agency.el
+++ local/enable/tools/ssh-agency.el
@@ -1,5 +1,7 @@
 ;;; ssh-agency.el -*- lexical-binding: t; -*-
 
+(enable-when (not rps-system-android-p))
+
 (cfg-pkg ssh-agency
   (:after magit (:require ssh-agency))
   (:hook-to 'vc-retrieve-tag
