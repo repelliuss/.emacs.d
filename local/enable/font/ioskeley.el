cdf7eb1973c3bf4f341478a1d032fdcfd374d7b1
repelliuss <repelliuss@gmail.com>
2026-09-15 22:27:29 +0300

    fix mode-line not being applied

--- local/enable/font/ioskeley.el
+++ local/enable/font/ioskeley.el
@@ -16,15 +16,18 @@
                                                                 :default-height 120)))
 
         (unless rps-system-android-p
-          (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular-ioskeley-mono)))
-
-        ;; BUG: doesn't work on emacs startup
-        ;; fontaine doesn't handle mode-line face but doom-modeline
-        ;; explicitly use it to define a blank seperator between left
-        ;; aligned and right aligned segments for inactive(?)
-        ;; window. this causes shift on right side of the mode line
-        ;; after changing selected window.
-        (:face mode-line (:family "Ioskeley Mono" :height 130))
+          (fontaine-set-preset (or (fontaine-restore-latest-preset) 'regular-ioskeley-mono))
+          
+          ;; fontaine doesn't handle mode-line face but doom-modeline
+          ;; explicitly use it to define a blank seperator between left
+          ;; aligned and right aligned segments for inactive(?)
+          ;; window. this causes shift on right side of the mode line
+          ;; after changing selected window.
+          (:face mode-line (:family "Ioskeley Mono" :height 130))
+          
+          ;; furthermore, spacious-padding-mode resets the mode-line for some reason
+          (:hook-to 'spacious-padding-mode
+            (lambda () (:face mode-line (:family "Ioskeley Mono" :height 130)))))
         
         (when rps-system-android-p
           (defun rps-font-set-ioskeley ()
